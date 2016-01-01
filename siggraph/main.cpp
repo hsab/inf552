8bec07903d0fe0f5bfd9f5c174b4a3f57cf61d76
tizot <masset.camille@gmail.com>
2016-01-01 16:43:44 +0100

    Initial commit

--- siggraph/main.cpp
+++ siggraph/main.cpp
@@ -6,10 +6,26 @@
 //  Copyright © 2016 Ecole polytechnique. All rights reserved.
 //
 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <fstream>
+
+#include "maxflow/graph.h"
+
+using namespace std;
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // Récupérer l'input
+    Mat input = imread("../strawberries.jpg");
+    imshow("Input", input); waitKey();
+    
+    // Déterminer la taille de l'output
+    // Tant que l'output n'est pas complet
+        // Positionner un premier patch
+        // Découper le patch par graphcut
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
